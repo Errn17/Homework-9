39cefc593308bd56a7b14ab09e3cd70efa67be17
Aaron <aaronkh19@gmail.com>
2020-06-23 19:18:14 -0600

    created questions and functions, now just need to get it to update the readme file

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
-const questions = [];
+//requiring all the needed packages and dependencies
+const inquirer = require("inquirer");
+const genMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs").promises;
 
-function writeToFile(fileName, data) {}
+//creating the questions to be asked when creating your readMe document
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your Project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Give a quick description of the project:",
+  },
+  {
+    type: "input",
+    name: "table of contents",
+    message: "Is their a table of contents for this project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage of the project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What packages were installed and used for this project?",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What licesnse were used for this project?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message:
+      "Did anyone else help or contribute to this project? If so please list their github profile:",
+  },
+  {
+    type: "input",
+    name: "username",
+    message: "What is you username for Github?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+];
 
-function init() {}
+//console logging to make sure I set up questions correctly
+console.log(questions);
+
+//Function for pushing the user input to the read me file
+function writeToFile(fileName, data) {
+  const content = genMarkdown(data);
+  fs.writeFile(fileName, content);
+}
+
+function init() {
+  try {
+    const userInput = inquirer.prompt(questions);
+    writeToFile("README.md", userInput);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 init();
